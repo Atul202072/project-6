8749d82dd31a9e305a9f33d44c2571d47d5c21ae
Atul <76253839+Atul202072@users.noreply.github.com>
2021-11-17 22:25:51 +0530

    Add files via upload
    swap by refrence

---
+++ main.cpp
@@ -0,0 +1,23 @@
+//call by refrence method
+#include <iostream>
+using namespace std;
+void swapPointer(int* a, int* b)
+{
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
+ int main()
+ {
+    int a,b;
+    cout<<"Enter the value of a: ";
+    cin>>a;
+    cout<<"Enter the value of b: ";
+    cin>>b;
+    cout<<"Before swapping: "<<endl;
+    cout<<"The value of a is: "<<a<<endl<<"The value of b is: "<<b<<endl;
+    swapPointer(&a,&b);
+    cout<<"After swapping: "<<endl;
+    cout<<"The value of a is: "<<a<<endl<<"The value of b is: "<<b<<endl;
+    return 0;
+ }
